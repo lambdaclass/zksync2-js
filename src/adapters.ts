aa6824fcdc134fa9fe4f18ab5f8de77e131b4578
Francisco Krause Arnim <fkrausear@gmail.com>
2024-02-26 18:33:13 -0300

    fix: remove wrong value = 0

--- src/adapters.ts
+++ src/adapters.ts
@@ -220,18 +220,13 @@ export function AdapterL1<TBase extends Constructor<TxSender>>(Base: TBase) {
 
                     const neededAllowance = baseCost.add(depositTx.l2Value).add(operatorTip);
 
-                    // const neededAllowance = depositTx.overrides.value;
-                    // if (currentAllowance.lt(neededAllowance)) {
-                    // const approveTx = await this.approveERC20(nativeERC20, neededAllowance, {
-                    //     bridgeAddress,
-                    //     ...transaction.approveOverrides,
-                    // });
-
-                    const approveTx = await this.approveERC20(nativeERC20, neededAllowance, {
-                        bridgeAddress,
-                        ...transaction.approveOverrides,
-                    });
-                    await approveTx.wait();
+                    if (currentAllowance.lt(neededAllowance)) {
+                        const approveTx = await this.approveERC20(nativeERC20, neededAllowance, {
+                            bridgeAddress,
+                            ...transaction.approveOverrides,
+                        });
+                        await approveTx.wait();
+                    }
                 }
 
                 const baseGasLimit = await this.estimateGasRequestExecute(depositTx, nativeERC20 == transaction.token);
@@ -778,7 +773,7 @@ export function AdapterL1<TBase extends Constructor<TxSender>>(Base: TBase) {
                 const conversionRate = await this._providerL2().getConversionRate();
                 baseCost = baseCost.mul(conversionRate);
 
-                overrides.value = 0;
+                // overrides.value = 0;
             } else {
                 await checkBaseCost(baseCost, overrides.value);
             }
